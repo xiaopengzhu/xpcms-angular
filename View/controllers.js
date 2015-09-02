d011b43a6a474ee55684bcdadc8117a5254391bc
zxp <zxp@yiban.cn>
2015-09-02 15:21:10 +0800

    1

--- View/controllers.js
+++ View/controllers.js
@@ -1,9 +1,33 @@
-/* 首页控制器 */
-function IndexCtrl($scope) {
+var app = angular.module('app',[]);
+
+app.controller('indexCtrl', ["$scope", "$http", function($scope, $http) {
     $scope.categorys = [
-        {id:1, name: '首页'},
         {id:2, name: '文章'},
         {id:3, name: '音乐'},
         {id:4, name: '视频'}
     ];
-}
+    
+    $http.get('/xpcms-angular/service/getCategorys')
+    .success(function (data) {
+        $scope.categorys = data;
+    })
+    .error(function () {
+        alert("get categorys error");
+    });
+    
+    $http.get('/xpcms-angular/service/getIndexData')
+    .success(function (data) {
+        $scope.data = data;
+    })
+    .error(function () {
+        alert("get slides error");
+    });
+    
+    $http.get('/xpcms-angular/service/getLink')
+    .success(function (data) {
+        $scope.links = data;
+    })
+    .error(function () {
+        alert("get links error");
+    });
+}]);
